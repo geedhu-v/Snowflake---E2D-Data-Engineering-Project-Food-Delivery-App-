62cc164d4f1d75a4e00edac30f6017b42114c0d4
Geedhu KV <153047532+geedhu-v@users.noreply.github.com>
2025-06-27 08:17:28 -0400

    Add files via upload

---
+++ Snowflake Native features/06-create-task-objects.sql
@@ -0,0 +1,62 @@
+-- Data Engineering Simplified YouTube Channel
+-- https://www.youtube.com/c/DataEngineeringSimplified
+
+-- Snowflake Tasks & Task Tree (Snowflake Hands-on Tutorial)  https://youtu.be/165hXZ_o4Vk
+-- CDC & Delta Processing In Snowflake (Snowflake Hands-on Tutorial) https://youtu.be/eJ7QCrDZDJo
+-- How to work with external table in Snowflake  (Snowflake Hands-on Tutorial) https://youtu.be/w9BQsOlJc5s
+-- Snowflake Unique Features (Snowflake Hands-on Tutorial) https://youtu.be/-MZrCpoAUs0
+-- Snowflake Must Know New Objects (Snowflake Hands-on Tutorial)  https://youtu.be/S5NwU2o2Exg
+
+
+-- change context
+use role sysadmin;          -- use sysadmin role
+use schema demo_db.public;  -- use default public schema
+use warehouse compute_wh;   -- use compute warehouse
+
+-- creating a simple task that runs every hours
+create or replace task task_a
+    warehouse = compute_wh
+    schedule = '1 minute'
+    as
+    select current_database();
+
+create or replace task task_b
+    warehouse = compute_wh
+    after task_a
+    as
+    select current_schema();
+
+create or replace task task_c
+    warehouse = compute_wh
+    after task_b
+    as
+    select current_warehouse();
+
+create or replace task task_d
+    warehouse = compute_wh
+    after task_b
+    as
+    select current_region();
+
+create or replace task task_e
+    warehouse = compute_wh
+    after task_c,task_d
+    as
+    call my_storedprocedure();
+
+-- resume all the task
+use role accountadmin;
+grant execute task, execute managed task on account to role sysadmin;
+use role sysadmin;
+
+alter task task_e resume;
+alter task task_d resume;
+alter task task_c resume;
+alter task task_b resume;
+alter task task_a resume;
+
+alter task task_a suspend;
+alter task task_b suspend;
+alter task task_c suspend;
+alter task task_d suspend;
+alter task task_e suspend;
